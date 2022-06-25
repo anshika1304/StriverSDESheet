68bdf4dda08e75d06c4ef0971d3fb02ca7c1fbff
anshika1304 <83755373+anshika1304@users.noreply.github.com>
2022-06-26 00:17:13 +0530

    Update TheN-thRootOfAnInteger.java

--- Day11/TheN-thRootOfAnInteger.java
+++ Day11/TheN-thRootOfAnInteger.java
@@ -1,20 +1,2 @@
 // https://bit.ly/3rj7Ib1
 
-public class Solution {
-    public static double findNthRootOfM(int n, long m) {
-    double start=1, end=m, mid=0;
-        while(start<end){
-            mid=start+(end-start)/2;
-            if(Math.pow(n,mid)==m){
-                return mid;
-            }
-            else if(Math.pow(n,mid)>m){
-                end=mid-1;
-            }
-            else{
-                start=mid+1;
-            }
-        }
-        return Math.pow(m, (double)1/n);
-    }
-}

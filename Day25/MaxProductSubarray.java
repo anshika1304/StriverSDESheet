f45de0517c8a1405e587b83460054c12971b4e97
anshika1304 <83755373+anshika1304@users.noreply.github.com>
2022-07-15 23:58:55 +0530

    Update MaxProductSubarray.java

--- Day25/MaxProductSubarray.java
+++ Day25/MaxProductSubarray.java
@@ -1,1 +1,24 @@
+// https://leetcode.com/problems/maximum-product-subarray/
 
+class Solution {
+    public int maxProduct(int[] nums) {
+        int ans=Integer.MIN_VALUE;
+        for(int i=0; i<nums.length; i++){
+            ans=Math.max(ans, nums[i]);
+        }
+        int currMin=1, currMax=1;
+        for(int n:nums){
+            if(n==0){
+                currMin=1;
+                currMax=1;
+                continue;
+            }
+            int temp1=n*currMax;
+            int temp2=n*currMin;
+            currMax=Math.max(n, Math.max(temp1,temp2));
+            currMin=Math.min(n, Math.min(temp1, temp2));
+            ans=Math.max(ans, currMax);
+        }
+        return ans;
+    }
+}
